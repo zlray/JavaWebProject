7c54c2f3b71a7ab361ef277d1bc276af66ee6174
zlray <1411826682@qq.com>
2018-11-25 22:23:00 +0800

    servlet Response Request Redirect Forward

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,28 +1,58 @@
+import pojo.User;
+import service.LoginService;
+import service.impl.LoginServiceImpl;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @description 浏览器到servlet -  service - dao - 操作数据库  双向
+ */
+
 public class LoginServlet extends HttpServlet {
 
+//    @Override
+//    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//        System.out.println("登陆的service");
+////        super.service(req,res); //一般不调用 避免出现405方法找不到（调用父类的方法必须写doGet()/doPost()）
+//    }
+
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("登陆的service");
-        super.service(req,res);
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        //2.处理请求乱码
+        req.setCharacterEncoding("utf-8");
+        //设置响应编码格式
+        resp.setContentType("text/html;charset=utf-8");
+        //获取请求的信息
+        String username = req.getParameter("username");
+        //转编码格式：第一种方式
+//        username = new String(username.getBytes("iso8859-1"),"utf-8");
+
+        String pwd = req.getParameter("password");
+        System.out.println("页面获取的： " + username + pwd);
+        //处理请求信息
+            //获取业务层对象
+        LoginService loginService = new LoginServiceImpl();
+
+        User user = loginService.checkLoginService(username, pwd);
+        System.out.println(user);
+        if (user!= null){
+            //请求转发
+            //使用request对象实现不同servlet的数据流传
+//            req.setAttribute("error","用户名或者密码错误");
+//            req.getRequestDispatcher("LoginDemo.jsp").forward(req,resp);
+            //请求重定向
+            resp.sendRedirect("RequestRedirectServlet.jsp");
+        }else {
+            //使用request对象实现不同servlet的数据流传
+            req.setAttribute("error","用户名或者密码错误");
+//            resp.getWriter().write("login fail");
+            req.getRequestDispatcher("LoginDemo.jsp").forward(req,resp);
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("我是login的post方法");
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("我是login的get方法");
-    }
 }
---
+++ web/LoginDemo.jsp
@@ -0,0 +1,45 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zl
+  Date: 2018/11/5
+  Time: 21:22
+  DescripTtion:JavaScript的用法
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style type="text/css">
+        #div1 {
+            width: 200px;
+            height: 200px;
+            border: 1px solid red;
+        }
+    </style>
+</head>
+<body>
+
+<form action="LoginServlet" method="get">
+    用户名：<input type="text" name="username" value=""> <br/>
+    密码：<input type="text" name="password" value=""> <br/>
+    爱好:<br/>
+    <input type="checkbox" name="fav" value="1"/>唱歌<br/>
+    <input type="checkbox" name="fav" value="2"/>跳舞<br/>
+    <input type="checkbox" name="fav" value="3"/>游泳<br/>
+    <input type="submit" value="登陆"/><br/>
+</form>
+
+<%
+    //servlet传递到另一个servlet
+    Object msg = "";
+    msg = request.getAttribute("error");
+    if (msg != null) {
+        //请求转发
+        System.out.println(msg);
+    }else {
+
+    }
+%>
+
+</body>
+</html>
